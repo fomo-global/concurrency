b3dd983dfac5279082c64606a527caa7eb0bbff6
fomo-global <devmarlen163@gmail.com>
2026-07-08 22:27:58 +0300

    add generator content

--- generator/generator.go
+++ generator/generator.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+func generator(nums ...int) <-chan int {
+	out := make(chan int)
+	go func() {
+		defer close(out)
+		for _, n := range nums {
+			out <- n
+		}
+	}()
+	return out
+}
+
 func main() {
-	fmt.Println("Hello World")
+	for n := range generator(1, 2, 3, 4, 5) {
+		fmt.Println(n)
+	}
 }
